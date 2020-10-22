abc30b9916f6fd3701543b859f9cc074c2337907
Juan Pablo <necrofear_0488@hotmail.com>
2020-10-22 17:52:02 -0500

    docs agregado

---
+++ docs/precache-manifest.9de25d6ddc9d2aad732c4558b1b05a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7eb2da0fa6fc8345f23d9779ea3d87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc1aba966cfe2bb1f70d",
+    "url": "/static/css/main.367cd8c3.chunk.css"
+  },
+  {
+    "revision": "2f23321b03c6510d4825",
+    "url": "/static/js/2.c7444719.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c7444719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1aba966cfe2bb1f70d",
+    "url": "/static/js/main.acdc389c.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
